42a42177381a2861325daf19bc3272cad6b0292e
Yonghwan SO <scinix@gmail.com>
2017-08-07 23:34:05 +0900

    initialize repository with module 'tracker'.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Gitwork
+
+My personal workspace for git, github and more.
+
+## Tools
+
+### Tracker (`_tracker/tracker.rb`)
+
+Tracking interesting repositories with simple ruby script.
+
+This script just clone forked repository into current directory and add
+upstream branch of forked repository first, then merge upstream branch
+into master branch of forked repository.
+
+
---
+++ _tracker/sync.rb
@@ -0,0 +1,102 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'colorize'
+require 'json'
+
+file = File.read('repos.json')
+wd = 'working'
+
+hash = JSON.parse(file)
+
+unless File.exist?("#{wd}")
+  Dir.mkdir "#{wd}"
+end
+
+for e in hash
+  origin = e['origin']
+  forked = e['forked']
+  proj = File.basename(forked).sub('.git','')
+  puts ""
+  puts ">>> processing #{proj} (#{forked})...".blue
+  if origin == ""
+    puts ">>> origin is blank. maybe break sign. abort processing.".magenta
+    break
+  end
+
+  # clone it if there is no cloned one.
+  unless File.exist?("#{wd}/#{proj}")
+    ret = system("cd #{wd}; git clone " + forked)
+    unless ret
+      puts ">>> cloning failed for #{proj}".red
+      next
+    end
+
+    ret = system("cd #{wd}/#{proj}; git remote add upstream #{origin}")
+    unless ret
+      puts ">>> cloned but cannot add upstream for #{proj}".red
+      next
+    end
+    puts ">>> cloning and adding upstream completed for #{proj}.".yellow
+  end
+
+  # preparing upstream (fetching...)
+  ret = system("cd #{wd}/#{proj}; git fetch upstream master")
+  unless ret
+    puts ">>> fetching upstream failed for #{forked}".red
+    next
+  end
+
+  # merging into upstream.
+  ret = system("cd #{wd}/#{proj}; git branch upstream")
+  ret = system("cd #{wd}/#{proj}; git checkout upstream")
+  unless ret
+    puts ">>> fetched but checking out upstream failed for #{proj}".red
+    next
+  end
+
+  output = `cd #{wd}/#{proj}; git merge upstream/master`
+  unless $?.success?
+    puts ">>> checked out but merging failed for #{proj}".red
+    next
+  end
+  puts ">>> checking out and merging upstream completed.".green
+
+  if output.strip == 'Already up-to-date.'
+    puts ">>> the branch already up-to-date!".yellow
+  else
+    ret = system("cd #{wd}/#{proj}; git push")
+    unless ret
+      puts ">>> pushing upstream failed for #{proj}".red
+      next
+    end
+    puts ">>> pushing upstream completed: #{proj}".green
+  end
+
+  # update master...
+  ret = system("cd #{wd}/#{proj}; git checkout master")
+  unless ret
+    puts ">>> checking out master failed for #{proj}".red
+    next
+  end
+
+  output = `cd #{wd}/#{proj}; git merge upstream`
+  unless $?.success?
+    puts ">>> checked out but merging failed for #{proj}".red
+    next
+  end
+  puts ">>> checking out and merging master completed.".green
+
+  if output.strip == 'Already up-to-date.'
+    puts ">>> the branch already up-to-date!".yellow
+  else
+    ret = system("cd #{wd}/#{proj}; git push")
+    unless ret
+      puts ">>> pushing master failed for #{proj}".red
+      next
+    end
+    puts ">>> pushing master completed: #{proj}".green
+  end
+
+  puts ">>> syncing #{forked} completed".blue
+end
